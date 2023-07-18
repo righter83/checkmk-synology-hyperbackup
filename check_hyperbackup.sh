ac36e9a215642c6bd79446f219736c329337009d
righter83 <reg@righter.ch>
2023-07-18 15:04:31 +0200

    Update check_hyperbackup.sh
    added version switch

--- check_hyperbackup.sh
+++ check_hyperbackup.sh
@@ -10,7 +10,16 @@ maxSecongsAge="86500" # How old can a backup be
 LANG=de_DE.UTF-8 # Locale Settings because of 12 or 24 hr calc
 LC_ALL=de_DE.UTF-8
 
-message=$(grep "$job_name" /var/log/messages | grep -w 'with result \[[1]\]' | tail -1)
+# check version of hyperbackup
+hbver=$(synopkg version HyperBackup)
+shortver=${hbver:0:1}${hbver:2:1}
+if [ "$shortver" -gt "40" ]
+then
+        message=$(grep "$job_name" /volume1/@appdata/HyperBackup/log/hyperbackup.log | grep -w 'with result [[1]]' | tail -1)
+else
+        message=$(grep "$job_name" /var/log/messages | grep -w 'with result \[[1]\]' | tail -1)
+fi
+
 detail=$(cut -c 92-200 <<< "$message")
 timeStamp=$(echo $message | grep -P '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]' -o)
 
